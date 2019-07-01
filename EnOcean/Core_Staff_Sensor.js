1d13dcf6a71c9d0b035acf8b7b1167edef9d14e0
gotoh-t <t.gotou@centurysys.co.jp>
2019-07-01 18:35:59 +0900

    Core-Staff温湿度センサーに対応

---
+++ EnOcean/Core_Staff_Sensor.js
@@ -0,0 +1,32 @@
+module.exports = function(RED) {
+    "use strict";
+    var settings = RED.settings;
+
+    // collect-data-object config node function definition
+    function Core_Staff_Sensor(config) {
+        RED.nodes.createNode(this,config);
+        this.sensor_id = config.sensor_id;
+        //this.dataname0 = config.dataname0;
+        //this.unitType0 = config.unitType0;
+        //this.dataname1 = config.dataname1;
+        //this.unitType1 = config.unitType1;
+
+        var node = this;
+        var confObj = config.configObject;
+        node.log("-----> confObj = " + confObj);
+        this.dItems = {};
+        if (confObj) {
+          try { this.dItems = JSON.parse(confObj); }
+          catch(e) {
+            // nodeのエラーを通知してして終了
+            node.error("runtime:jsonerror", confObj);
+          }
+        } else {
+            // nodeのエラーを通知してして終了
+            node.error("runtime:jsonerror", confObj);
+        }
+        this.on("input",function(msg) {});
+        this.on("close",function() {});
+    }
+    RED.nodes.registerType("Core_Staff_Sensor",Core_Staff_Sensor);
+}
--- EnOcean/sensor.js
+++ EnOcean/sensor.js
@@ -1,7 +1,7 @@
 // 各センサー毎の測定値計算モジュールをここに定義する
 
 // 登録されているセンサーの計算モジュールリスト
-module.exports.module_list = {"u-rd":"calc_ac", "watty":"calc_temperature"};
+module.exports.module_list = {"u-rd":"calc_ac", "watty":"calc_temperature", "core_staff":"calc_temp_humidity"};
 
 // 温度計算（Watty）
 module.exports.calc_temperature = function (data){
@@ -67,4 +67,36 @@ module.exports.calc_ac = function (data){
     acList.push(ac);
 
     return acList;
+};
+
+// 温湿度計算（Core Staff）
+module.exports.calc_temp_humidity = function (data){
+    var result = [];
+    if (data.length < 4*2) {
+        // 4Byte以上でなければ空リスト返却
+        return ret;
+    }
+    // 4Byteのデータ長のうち先頭2Byte目が湿度、3Byte目が温度
+    var dec = parseInt(data, 16);
+    //var bin = dec.toString(2);
+    // 湿度の抽出(2Byte目)
+    //var dec1 = parseInt(bin.substr(8,8),2);
+    var dec1 = (dec >> 16) & 0xFF;
+    // 温度の抽出(3Byte目)
+    //var dec2 = parseInt(bin.substr(16,8),2);
+    var dec2 = (dec >> 8) & 0xFF;
+    
+    // 湿度、温度の計算（0～250の数値を0～100%、-20～60℃に変換する)
+    var hid = dec1 * (100 / 250);
+    var temp = dec2 * (80 / 250) - 20;
+    // 誤差を丸める
+    hid = Math.round(hid * 10);
+    hid = hid / 10;
+    temp = Math.round(temp * 100);
+    temp = temp / 100;
+    
+    result.push(hid);
+    result.push(temp);
+    
+    return result;
 };
