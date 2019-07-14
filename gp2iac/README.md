6fe8dbadac1e02ec1eacd2a45dc04e3e510ccaf8
mitutake <mitutake@gmail.com>
2019-07-14 18:33:50 +0900

    gp2iac ノード 初回コミット ia-cloud-cnctでDB登録まで可能

---
+++ gp2iac/README.md
@@ -0,0 +1,81 @@
+# GrovePI Node とia-cloud-cnct Node をつなぐNode
+
+## gp2iac
+
+## 機能概要
+
+このノードは、RaspberryPIに接続されたGrove Piの各種センサーの値を取得する「node-red-contrib-grovepi」の値を、「ia-cloud-cnct」で処理できる形に変換するノードです。
+
+#### 動作方法について：
+
+- 本ノードに接続している「node-red-contrib-grovepi」で取得するセンサーの種類をプロパティの「Sensor Type」で指定する。
+- 入力された値は「ia-cloud-cnct」様式のjasonに変換して出力される。
+
+## 入力メッセージ
+
+* DHT11(温湿度センサー)：
+* Ultrasonic Range(超音波測距センサー)：
+* Button(Buttonセンサー)：
+
+
+
+## プロパティー
+
+本Nodeは以下のプロパティを持つ
+
+| 名称       |  種別  | 説明                 |
+| ---------- | :----: | -------------------- |
+| name       | string | Nodeの名称           |
+| user       | string | 利用者               |
+| SensorType | string | センサータイプを選択 |
+
+
+
+## 出力メッセージ
+
+* ``msg``:  **object**
+  * 取得した値を配列に格納する。
+
+| 名称       | 種別   | 説明                                                         |
+| ---------- | ------ | ------------------------------------------------------------ |
+| timestamp  | string | タイムスタンプ(isodatetime)<br />※入力メッセージのtimestampではなく、本ノード内で新たに取得したtimestamp。 |
+| user       | string | 利用者                                                       |
+| sensortype | string | 選択されたセンサーのタイプ<br />    DHT11(温湿度センサー)：dht11<br />    Ultrasonic Range(超音波測距センサー)：ultrasonic<br />    Button(Buttonセンサー)：button |
+| dataname   | string | センサータイプに応じた取得データ名称                         |
+| datavalue  | string | センサーが取得した値                                         |
+| unit       | string | センサーが取得した値に応じた単位                             |
+
+
+```
+msg = {
+    "request": "store",
+    "dataObject": {
+        "objectType" : "iaCloudObject",
+        "objectKey" : "ia-cloud-Node-Red-HandsOn." + {user} + "." + {sensortype} ,
+        "objectDescription" : "センサーの値",
+        "timeStamp" :  {timestamp},
+        "ObjectContent" : {
+            "contentType": "com.ia-cloud.contenttype.hackathon2017.temp01",
+            "contentData":[{
+                "commonName": "Column1",
+                "dataName": "{dataname}",
+                "dataValue": {datavalue},
+                "unit": "{unit}"
+            },{
+                "commonName": "Column2",
+                "dataName": "{dataname}",
+                "dataValue": {datavalue},
+                "unit": "{unit}"
+            },{
+	          	・
+	       	    ・
+	            ・
+	            ・
+            }]
+        }
+    }
+}
+
+
+```
+
---
+++ gp2iac/gp2iac.js
@@ -0,0 +1,108 @@
+module.exports = function(RED) {
+    "use strict";
+
+    function Gp2iac(config) {
+        RED.nodes.createNode(this,config);
+        this.name = config.neme;
+        this.user = config.user;
+        this.sensortype = config.sensortype;
+        var user = this.user;
+        var sensortype = this.sensortype;
+        var node = this;
+        var dataname = new Array(3);
+        var datavalue = new Array(3);
+        var unit = new Array(3);
+
+        //イベント：msg入力が取得トリガー
+        node.on('input', function(msg) {
+            if(sensortype == "dht11"){
+                dataname[0] = "temperature";
+                dataname[1] = "humidity";
+                dataname[2] = "heatIndex";
+                datavalue[0] = msg.payload.temperature;
+                datavalue[1] = msg.payload.humidity;
+                datavalue[2] = msg.payload.heatIndex;
+                unit[0] = "℃";
+                unit[1] = "%";
+                unit[2] = "HI";
+            }else if(sensortype == "button"){
+                dataname[0] = "button";
+                datavalue[0] = msg.payload;
+                unit[0] = "is_pressed";
+                dataname[1] = "dummy";
+                datavalue[1] = 0;
+                unit[1] = "value";
+                dataname[2] = "dummy";
+                datavalue[2] = 0;
+                unit[2] = "value";
+            }else{
+                dataname[0] = "ultrasonic";
+                datavalue[0] = msg.payload;
+                unit[0] = "cm";
+                dataname[1] = "dummy";
+                datavalue[1] = 0;
+                unit[1] = "value";
+                dataname[2] = "dummy";
+                datavalue[2] = 0;
+                unit[2] = "value";
+            }
+            Gp2iacMsg(msg);
+        });
+
+        //変換ファンクション
+        function Gp2iacMsg(msg){
+
+            var dateformat = require('dateformat');
+            var date = new Date();
+            var timestamp = dateformat(date, 'isoDateTime');
+
+            msg = {
+                "request": "store",
+                "dataObject": {
+                    "objectType" : "iaCloudObject",
+                    "objectKey" : "ia-cloud-Node-Red-HandsOn." + user + "." + sensortype ,
+                    "objectDescription" : "センサーの値",
+                    "timeStamp" :  timestamp,
+                    "ObjectContent" : {
+                        "contentType": "com.ia-cloud.contenttype.hackathon2017.temp01",
+                        "contentData":[{
+                            "commonName": "Column1",
+                            "dataName": dataname[0],
+                            "dataValue": datavalue[0],
+                            "unit": unit[0]
+                        },{
+                            "commonName": "Column2",
+                            "dataName": dataname[1],
+                            "dataValue": datavalue[1],
+                            "unit": unit[1]
+                        },{
+                            "commonName": "Column3",
+                            "dataName": dataname[2],
+                            "dataValue": datavalue[2],
+                            "unit": unit[2]
+                        },{
+                            "commonName": "Column4",
+                            "dataName": "ダミー",
+                            "dataValue": 0,
+                            "unit": "value"
+                        },{
+                            "commonName": "Column5",
+                            "dataName": "ダミー",
+                            "dataValue": 0,
+                            "unit": "value"
+                        },{
+                            "commonName": "Column6",
+                            "dataName": "ダミー",
+                            "dataValue": 0,
+                            "unit": "value"
+                        }]
+                    }
+                }
+                
+            }
+            node.send(msg);
+        }
+    }
+    RED.nodes.registerType('gp2iac',Gp2iac);
+}
+
