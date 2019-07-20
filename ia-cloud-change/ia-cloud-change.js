df95270819f8c988b27d161727680a054f54f733
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2019-07-20 23:53:38 +0900

    :+1: Implements ia-cloud-change node.

---
+++ ia-cloud-change/ia-cloud-change.js
@@ -0,0 +1,100 @@
+const moment = require('moment');
+
+/**
+ * msg.payload.temperatureのような値の取り方を解決する.
+ */
+const resolvePayload = (msg, key) => {
+  const keyArray = key.split('.');
+  if (keyArray.length === 1) {
+    return msg[keyArray.shift()];
+  }
+  return resolvePayload(msg[keyArray.shift()], keyArray.join('.'));
+};
+
+/**
+ * exportsするfunctionの定義.
+ * @see https://nodered.jp/docs/creating-nodes/node-js
+ * @param {object} RED - Node-REDで決められているお作法.
+ */
+function exportsFunction(RED) {
+  /**
+   * registerTypeで登録するfunctionの定義.
+   * @see https://nodered.jp/docs/creating-nodes/node-js#ノードコンストラクタ
+   * @param {object} config
+   */
+  function nodeFunction(config) {
+    const thisNode = this;
+    RED.nodes.createNode(thisNode, config);
+
+    // inputイベント(ノードがメッセージを受信)へのリスナー登録.
+    this.on('input', async (msg) => {
+      if (!config.rules || config.rules.length === 0) {
+        // No change rule configured.
+        return;
+      }
+
+      if (typeof msg.payload === 'object') {
+        // ex. msg.payload = {};
+      } else {
+        // ex. msg.payload = 46;
+      } 
+
+      // Change values with rules.
+      const contentData = config.rules
+        .filter(rule => rule.value && rule.dataName)
+        .map((rule) => {
+          const contentDatum = {
+            dataValue: resolvePayload(msg, rule.value),
+            dataName: rule.dataName,
+            commonName: rule.commonName,
+          };
+          if (rule.unit) { contentDatum.unit = rule.unit; }
+          return contentDatum;
+        });
+
+      // Variables.
+      // const serviceID = 'serviceID';
+      const objectKey = config.objectKey;
+      const objectDescription = config.objectDescription;
+      const timestamp = moment().format();
+      const instanceKey = config.instanceKey;
+
+      msg = {
+        request: 'store',
+        // serviceID,
+        dataObject: {
+          objectType: 'iaCloudObject',
+          objectKey,
+          objectDescription,
+          timestamp,
+          instanceKey,
+          objectContent: {
+            contentType: 'iaCloudData',
+            contentData,
+          },
+        }
+      };
+
+      thisNode.send(msg);
+    });
+
+    /**
+     * closeイベント(ノードの編集ダイアログを閉じる時)へのリスナー登録.
+     * @see https://nodered.jp/docs/creating-nodes/node-js#ノードを閉じる
+     */
+    thisNode.on('close', function(removed, done) {
+      if (removed) {
+        // This node has been deleted
+      } else {
+        // This node is being restarted
+      }
+      done();
+    });
+  }
+
+  RED.nodes.registerType('ia-cloud-change', nodeFunction, {
+    // credentials
+  });
+}
+
+module.exports = exportsFunction;
