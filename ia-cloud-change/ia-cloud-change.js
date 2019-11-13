9dd5c6eefa4c9add52db6402a5d1e34f4d450f01
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2019-11-14 08:06:25 +0900

    :recycle: Use 4 spaces for indent.

--- ia-cloud-change/ia-cloud-change.js
+++ ia-cloud-change/ia-cloud-change.js
@@ -4,11 +4,11 @@ const moment = require('moment');
  * msg.payload.temperatureのような値の取り方を解決する.
  */
 const resolvePayload = (msg, key) => {
-  const keyArray = key.split('.');
-  if (keyArray.length === 1) {
-    return msg[keyArray.shift()];
-  }
-  return resolvePayload(msg[keyArray.shift()], keyArray.join('.'));
+    const keyArray = key.split('.');
+    if (keyArray.length === 1) {
+        return msg[keyArray.shift()];
+    }
+    return resolvePayload(msg[keyArray.shift()], keyArray.join('.'));
 };
 
 /**
@@ -17,82 +17,82 @@ const resolvePayload = (msg, key) => {
  * @param {object} RED - Node-REDで決められているお作法.
  */
 function exportsFunction(RED) {
-  /**
-   * registerTypeで登録するfunctionの定義.
-   * @see https://nodered.jp/docs/creating-nodes/node-js#ノードコンストラクタ
-   * @param {object} config
-   */
-  function nodeFunction(config) {
-    const thisNode = this;
-    RED.nodes.createNode(thisNode, config);
+    /**
+     * registerTypeで登録するfunctionの定義.
+     * @see https://nodered.jp/docs/creating-nodes/node-js#ノードコンストラクタ
+     * @param {object} config
+     */
+    function nodeFunction(config) {
+        const thisNode = this;
+        RED.nodes.createNode(thisNode, config);
 
-    // inputイベント(ノードがメッセージを受信)へのリスナー登録.
-    this.on('input', async (msg) => {
-      if (!config.rules || config.rules.length === 0) {
-        // No change rule configured.
-        return;
-      }
+        // inputイベント(ノードがメッセージを受信)へのリスナー登録.
+        this.on('input', async (msg) => {
+            if (!config.rules || config.rules.length === 0) {
+                // No change rule configured.
+                return;
+            }
 
-      if (typeof msg.payload === 'object') {
-        // ex. msg.payload = {};
-      } else {
-        // ex. msg.payload = 46;
-      } 
+            if (typeof msg.payload === 'object') {
+                // ex. msg.payload = {};
+            } else {
+                // ex. msg.payload = 46;
+            }
 
-      // Change values with rules.
-      const contentData = config.rules
-        .filter(rule => rule.value && rule.dataName)
-        .map((rule) => {
-          const contentDatum = {
-            dataValue: resolvePayload(msg, rule.value),
-            dataName: rule.dataName,
-            commonName: rule.commonName,
-          };
-          if (rule.unit) { contentDatum.unit = rule.unit; }
-          return contentDatum;
+            // Change values with rules.
+            const contentData = config.rules
+                .filter((rule) => rule.value && rule.dataName)
+                .map((rule) => {
+                    const contentDatum = {
+                        dataValue: resolvePayload(msg, rule.value),
+                        dataName: rule.dataName,
+                        commonName: rule.commonName,
+                    };
+                    if (rule.unit) { contentDatum.unit = rule.unit; }
+                    return contentDatum;
+                });
+
+            // Variables
+
+            const dataObject = {
+                objectType: 'iaCloudObject',
+                objectKey: config.objectKey,
+                timestamp: moment().format(),
+                objectContent: {
+                    contentType: 'iaCloudData',
+                    contentData,
+                },
+            };
+            // Optional keys.
+            if (config.objectDescription) { dataObject.objectDescription = config.objectDescription; }
+            if (config.instanceKey) { dataObject.instanceKey = config.instanceKey; }
+
+            msg = {
+                request: 'store',
+                // serviceID,
+                dataObject,
+            };
+
+            thisNode.send(msg);
         });
 
-      // Variables
+        /**
+         * closeイベント(ノードの編集ダイアログを閉じる時)へのリスナー登録.
+         * @see https://nodered.jp/docs/creating-nodes/node-js#ノードを閉じる
+         */
+        thisNode.on('close', function (removed, done) {
+            if (removed) {
+                // This node has been deleted
+            } else {
+                // This node is being restarted
+            }
+            done();
+        });
+    }
 
-      const dataObject = {
-        objectType: 'iaCloudObject',
-        objectKey: config.objectKey,
-        timestamp: moment().format(),
-        objectContent: {
-          contentType: 'iaCloudData',
-          contentData,
-        },
-      };
-      // Optional keys.
-      if (config.objectDescription) { dataObject.objectDescription = config.objectDescription; }
-      if (config.instanceKey) { dataObject.instanceKey = config.instanceKey; }
-
-      msg = {
-        request: 'store',
-        // serviceID,
-        dataObject,
-      };
-
-      thisNode.send(msg);
+    RED.nodes.registerType('ia-cloud-change', nodeFunction, {
+        // credentials
     });
-
-    /**
-     * closeイベント(ノードの編集ダイアログを閉じる時)へのリスナー登録.
-     * @see https://nodered.jp/docs/creating-nodes/node-js#ノードを閉じる
-     */
-    thisNode.on('close', function(removed, done) {
-      if (removed) {
-        // This node has been deleted
-      } else {
-        // This node is being restarted
-      }
-      done();
-    });
-  }
-
-  RED.nodes.registerType('ia-cloud-change', nodeFunction, {
-    // credentials
-  });
 }
 
 module.exports = exportsFunction;
