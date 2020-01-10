d7d5d6ea91135263f774e12de4f58d0c1e87e0a4
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2020-01-11 01:30:55 +0900

    :bug: Fix ESLint warning; no-var.

--- EnOcean/sensor.js
+++ EnOcean/sensor.js
@@ -19,22 +19,22 @@ module.exports.calc_temperature = function (data) {
     }
     // javascriptでは32bit以上の数値をビットシフトできないため
     // 数値を10bit毎に分割してから計算する
-    var dec = parseInt(data, 16);
-    var bin = dec.toString(2);
-    var dec1 = parseInt(bin.substr(0, 10), 2);
-    var dec2 = parseInt(bin.substr(10, 10), 2);
+    const dec = parseInt(data, 16);
+    const bin = dec.toString(2);
+    const dec1 = parseInt(bin.substr(0, 10), 2);
+    const dec2 = parseInt(bin.substr(10, 10), 2);
 
-    var dec3 = parseInt(bin.substr(20, 10), 2);
-    var dec4 = parseInt(bin.substr(30, 10), 2);
-    var decList = [];
+    const dec3 = parseInt(bin.substr(20, 10), 2);
+    const dec4 = parseInt(bin.substr(30, 10), 2);
+    const decList = [];
     decList.push(dec1);
     decList.push(dec2);
     decList.push(dec3);
     decList.push(dec4);
 
-    var tempList = [];
-    for (var ch_val of decList) {
-        var temp = 130.0 - (parseFloat(ch_val) / 1024.0 * 170.0);
+    const tempList = [];
+    for (const ch_val of decList) {
+        const temp = 130.0 - (parseFloat(ch_val) / 1024.0 * 170.0);
         tempList.push(temp);
     }
     return tempList;
@@ -42,16 +42,16 @@ module.exports.calc_temperature = function (data) {
 
 // 電流計算（UR-D）
 module.exports.calc_ac = function (data) {
-    var ret = [];
+    const ret = [];
     if (data.length < 4 * 2) {
         // 4Byte以上でなければ空リスト返却
         return ret;
     }
-    var dec = parseInt(data, 16);
-    var acList = [];
-    var ad_val = (dec >> 8) & 0b1111111111;
+    const dec = parseInt(data, 16);
+    const acList = [];
+    const ad_val = (dec >> 8) & 0b1111111111;
 
-    var K = 0;
+    let K = 0;
     if (ad_val < 9) {
         K = (-0.0448 * ad_val) + 1.77;
     } else if (ad_val >= 9 && ad_val < 20) {
@@ -64,13 +64,13 @@ module.exports.calc_ac = function (data) {
         K = (0.000365 * ad_val) + 0.86;
     }
 
-    var E = 1.76;
+    const E = 1.76;
     // CT径が10mm なのでc, d は以下の数値
-    var c = 56;
-    var d = 3000;
+    const c = 56;
+    const d = 3000;
 
-    var I = (ad_val * K * E * d) / (2.8 * c);
-    var ac = I / 1000;
+    const I = (ad_val * K * E * d) / (2.8 * c);
+    const ac = I / 1000;
     acList.push(ac);
 
     return acList;
@@ -78,21 +78,21 @@ module.exports.calc_ac = function (data) {
 
 // 温湿度計算（Core Staff）
 module.exports.calc_temp_humidity = function (data) {
-    var result = [];
+    const result = [];
     if (data.length < 4 * 2) {
         // 4Byte以上でなければ空リスト返却
         return result;
     }
     // 4Byteのデータ長のうち先頭2Byte目が湿度、3Byte目が温度
-    var dec = parseInt(data, 16);
+    const dec = parseInt(data, 16);
     // 湿度の抽出(2Byte目)
-    var dec1 = (dec >> 16) & 0xFF;
+    const dec1 = (dec >> 16) & 0xFF;
     // 温度の抽出(3Byte目)
-    var dec2 = (dec >> 8) & 0xFF;
+    const dec2 = (dec >> 8) & 0xFF;
 
     // 湿度、温度の計算（0～250の数値を0～100%、-20～60℃に変換する)
-    var hid = dec1 * (100 / 250);
-    var temp = dec2 * (80 / 250) - 20;
+    let hid = dec1 * (100 / 250);
+    let temp = dec2 * (80 / 250) - 20;
     // 誤差を丸める
     hid = Math.round(hid * 10);
     hid = hid / 10;
@@ -107,22 +107,21 @@ module.exports.calc_temp_humidity = function (data) {
 
 // 電流計算（ITEC）
 module.exports.calc_itec_ct = function (data) {
-    var result = [];
+    const result = [];
     if (data.length < 3 * 2) {
         // 3Byte以上でなければ空リスト返却
         return result;
     }
-    var dec = parseInt(data, 16);
-    var bin = dec.toString(2);
-    var bin = ('000000000000000000000000' + bin).slice(-24); // 0パディング（24桁）
+    const dec = parseInt(data, 16);
+    const bin = `000000000000000000000000${dec.toString(2)}`.slice(-24); // 0パディング（24桁）
     // Divisor（先頭から2bit目)の値を取得する
-    var div = parseInt(bin.substr(1, 1), 2);
+    const div = parseInt(bin.substr(1, 1), 2);
 
     // 1CH分の値とDivisor（及びPower Fail）を取得
-    var div_ch = dec >> 4;
+    const div_ch = dec >> 4;
 
     // CH1の値
-    var ch1 = div_ch & 0xFFF;
+    const ch1 = div_ch & 0xFFF;
 
     if (div == 1) {
         // Scaleが10分の1
@@ -137,24 +136,23 @@ module.exports.calc_itec_ct = function (data) {
 
 // ロッカースイッチの状況取得（OPTEX）
 module.exports.get_rocker_sw = function (data) {
-    var result = [];
+    const result = [];
     if (data.length < 2) {
         // 1Byte以上でなければ空リスト返却
         return result;
     }
-    var dec = parseInt(data, 16);
-    var bin = dec.toString(2);
-    var bin = ('00000000' + bin).slice(-8); // 0パディング（8桁）
+    const dec = parseInt(data, 16);
+    const bin = `00000000${dec.toString(2)}`.slice(-8); // 0パディング（8桁）
     // State of the energy bow
-    var ebo = parseInt(bin.substr(0, 1), 2);
+    const ebo = parseInt(bin.substr(0, 1), 2);
     // State I of rocker B
-    var rbi = parseInt(bin.substr(4, 1), 2);
+    const rbi = parseInt(bin.substr(4, 1), 2);
     // State O of rocker B
-    var rbo = parseInt(bin.substr(5, 1), 2);
+    const rbo = parseInt(bin.substr(5, 1), 2);
     // State I of rocker A
-    var rai = parseInt(bin.substr(6, 1), 2);
+    const rai = parseInt(bin.substr(6, 1), 2);
     // State O of rocker A
-    var rao = parseInt(bin.substr(7, 1), 2);
+    const rao = parseInt(bin.substr(7, 1), 2);
 
     if (ebo == 1) {
         result.push('pressed');
@@ -187,29 +185,29 @@ module.exports.get_rocker_sw = function (data) {
 
 // 在室センサーの状況取得（OPTEX）
 module.exports.get_occupancy = function (data) {
-    var result = [];
+    const result = [];
     if (data.length < 4 * 2) {
         // 4Byte以上でなければ空リスト返却
         return result;
     }
 
     // 4Byteのデータ長のうち先頭1Byte目が供給電圧、3Byte目が在室状態
-    var dec = parseInt(data, 16);
+    const dec = parseInt(data, 16);
     // 供給電圧の抽出(1Byte目)
-    var dec1 = (dec >> 24) & 0xFF;
+    const dec1 = (dec >> 24) & 0xFF;
     // 在室状態の抽出(3Byte目)
-    var dec2 = (dec >> 8) & 0xFF;
+    const dec2 = (dec >> 8) & 0xFF;
     // 供給電圧利用可否フラグ
-    var is_supply = dec & 0x01;
+    const is_supply = dec & 0x01;
 
     // 供給電圧の計算（0～250の数値を0～5.0Vに変換する)
-    var volt = dec1 * (5 / 250);
+    let volt = dec1 * (5 / 250);
     // 誤差を丸める
     volt = Math.round(volt * 100);
     volt = volt / 100;
 
     // 在室状態
-    var occupancy = '';
+    let occupancy = '';
     if (dec2 < 128) {
         occupancy = '不在です'; // 不在
     } else {
