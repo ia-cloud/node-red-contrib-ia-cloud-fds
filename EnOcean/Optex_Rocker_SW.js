919372879de39b38961485f36d7454c29033aaa2
Ryoichi Obara <ryoichi-obara@users.noreply.github.com>
2020-01-16 14:27:35 +0900

    :art: Fix ESLint warnings.
    - space-before-function-paren
    - quotes
    - no-unused-vars
    - comma-spacing
    - keyword-spacing
    - indent
    - brace-style
    - semi
    - eol-last

--- EnOcean/Optex_Rocker_SW.js
+++ EnOcean/Optex_Rocker_SW.js
@@ -1,29 +1,27 @@
-module.exports = function(RED) {
-    "use strict";
-    var settings = RED.settings;
+module.exports = function (RED) {
+    'use strict';
 
     // collect-data-object config node function definition
     function Optex_Rocker_SW(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         this.sensor_id = config.sensor_id;
-        //this.dataName0 = config.dataName0;
-        //this.unitType0 = config.unitType0;
 
         var node = this;
         var confObj = config.configObject;
         this.dItems = {};
         if (confObj) {
-          try { this.dItems = JSON.parse(confObj); }
-          catch(e) {
-            // nodeのエラーを通知してして終了
-            node.error("runtime:jsonerror", confObj);
-          }
+            try {
+                this.dItems = JSON.parse(confObj);
+            } catch (e) {
+                // nodeのエラーを通知してして終了
+                node.error('runtime:jsonerror', confObj);
+            }
         } else {
             // nodeのエラーを通知してして終了
-            node.error("runtime:jsonerror", confObj);
+            node.error('runtime:jsonerror', confObj);
         }
-        this.on("input",function(msg) {});
-        this.on("close",function() {});
+        this.on('input', function (msg) {});
+        this.on('close', function () {});
     }
-    RED.nodes.registerType("Optex_Rocker_SW",Optex_Rocker_SW);
-}
+    RED.nodes.registerType('Optex_Rocker_SW', Optex_Rocker_SW);
+};
