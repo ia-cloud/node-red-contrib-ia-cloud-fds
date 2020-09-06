2da6b4df26fde59695e822962fd577caa898ef63
hashisan-atBridge <consulting@hashimukai.com>
2020-09-06 10:36:48 +0900

    added a new ia-cloud connection node for the request package deprecation
    httpライブラリー"request"のdeprecteに伴い、他のライブラリーを利用する新規Nodewo追加。内部を構造化（クラス化）し、異なるライブラリーを使用する場合に変更すべきメソッドを一つのみとした。
    今回の実装にあたっては、"got"ライブラリーを使用。
    https://github.com/sindresorhus/got

---
+++ ia-cloud-cnct/ia-cloud-cnct2.js
@@ -0,0 +1,219 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+"use strict";
+
+module.exports = function(RED) {
+
+    const iaCloudConnection = require("./util/ia-cloud-connection.js");
+
+    function iaCloudCnct2(config) {
+        RED.nodes.createNode(this,config);
+
+        let node = this;
+        let cnctRtryId;     // connect retry timer ID
+        let tappTimerId;    // tapping CCS (getStatus()) interval timer ID
+        
+        // ia-cloud connection config node instance
+        const ccsConnectionConfigNode = RED.nodes.getNode(config.ccsConnectionConfig);
+
+        // 接続情報を保持するオブジェクト
+        let info = {
+            status: "Disconnected",
+            serviceID: "",
+            url: ccsConnectionConfigNode.url,
+        //    userID: ccsConnectionConfigNode.credentials.userId,
+            FDSKey: config.FDSKey,
+            FDSType: "iaCloudFDS",
+            cnctTs:"",
+            lastReqTs: "",
+            comment: config.comment,
+            cnctRetryInterval: config.cnctRetryInterval * 60 * 1000,
+            tappingInterval: config.tappingInterval * 60 * 60 * 1000,
+
+            proxy: null,
+            reqTimeout: 12000
+        };
+
+        let auth = {
+            user: ccsConnectionConfigNode.credentials.userId,
+            pass: ccsConnectionConfigNode.credentials.password,
+        };
+
+        // proxy設定を取得
+        let prox;
+        let noprox;
+        if (process.env.http_proxy != null) { prox = process.env.http_proxy; }
+        if (process.env.HTTP_PROXY != null) { prox = process.env.HTTP_PROXY; }
+        if (process.env.no_proxy != null) { noprox = process.env.no_proxy.split(","); }
+        if (process.env.NO_PROXY != null) { noprox = process.env.NO_PROXY.split(","); }
+
+        let noproxy;
+
+        if (noprox) {
+            for (let i in noprox) {
+                if (info.url.indexOf(noprox[i]) !== -1) { noproxy=true; }
+            }
+        }
+        if (prox && !noproxy) {
+            let match = prox.match(/^(http:\/\/)?(.+)?:([0-9]+)?/i);
+            if (match) {
+                info.proxy = prox;
+            } else {
+                node.warn("Bad proxy url: "+ prox);
+                info.proxy = null;
+            }
+        }
+
+        // このタイムアウトの設定の詳細を調査する必要あり
+        if (RED.settings.httpRequestTimeout) {
+            info.reqTimeout = parseInt(RED.settings.httpRequestTimeout) || 120000;
+        }
+        else { info.reqTimeout = 120000; }
+
+        let cnctInfoName = "ia-cloud-connection-" + info.FDSKey;
+        let fContext = this.context().flow;
+        fContext.set(cnctInfoName, info);
+
+        const iaC = new iaCloudConnection(fContext, cnctInfoName);
+
+        //connect request を送出（接続状態にないときは最大cnctRetryIntervalで繰り返し）
+
+        let rInt = 3 * 60 * 1000;   //リトライ間隔の初期値3分
+        // connectリクエストのトライループ
+        (function cnctTry() {
+
+            //非接続状態なら接続トライ
+            if (info.status === "Disconnected") {
+
+                // node status をconnecting に
+                node.status({fill:"blue",shape:"dot",text:"runtime.connecting"});
+
+                // nodeの出力メッセージ（CCS接続状態）
+                let msg = {};
+                (async () => {
+                    // connect リクエスト
+                    try {
+                        let res = await iaC.connect(auth);
+                        node.status({fill:"green", shape:"dot", text:"runtime.connected"});
+                        msg.payload = res;
+                    } catch (error) {
+                        node.status({fill:"yellow", shape:"ring", text:error.message});
+                        msg.payload = error.message;
+                    } finally {
+                        node.send(msg);
+                    }
+                })();
+            }
+            //retryの設定。倍々で間隔を伸ばし最大はcnctRetryInterval、
+            if (info.cnctRetryInterval !== 0) {
+                cnctRtryId = setTimeout(cnctTry, rInt);
+                rInt *= 2;
+                rInt = (rInt < info.cnctRetryInterval)? rInt: info.cnctRetryInterval;
+            }
+        }());
+
+        if (info.tappingInterval !== 0) {
+            tappTimerId = setInterval(function(){
+
+                //非接続状態の時は、何もしない。
+                if (info.status === "Disconnected") return;
+
+                // node status をconnecting に
+                node.status({fill:"blue",shape:"dot",text:"runtime.connecting"});
+                info.status = "requesting";
+                let msg = {};
+                (async () => {
+                    // getStatus リクエスト
+                    try {
+                        let res = await iaC.getStatus(auth);
+                        node.status({fill:"green", shape:"dot", text:"runtime.connected"});
+                        msg.payload = res;
+                    } catch (error) {
+                        node.status({fill:"yellow", shape:"ring", text:error.message});
+                        msg.payload = error.message;
+                    } finally {
+                        node.send(msg);
+                    }
+                })();
+            }, info.tappingInterval) ;
+        }
+
+        this.on("input",function(msg) {
+
+            info = fContext.get(cnctInfoName);
+
+            //非接続状態の時は、何もしない。
+            if (info.status === "Disconnected") return;
+            
+            if (msg.request === "store"
+                || msg.request === "retrieve" || msg.request === "convey"){
+
+                // node status をReqesting に
+                node.status({fill:"blue", shape:"dot", text:"runtime.requesting"});
+                info.status = "requesting";
+
+                let dataObject = msg.dataObject;
+                (async () => {
+                    // リクエスト
+                    try {
+                        let res;
+                        if (msg.request === "store") res = await iaC.store(auth, dataObject);
+                        if (msg.request === "retrieve") res = await iaC.retrieve(auth);
+                        if (msg.request === "convey") res = await iaC.convey(auth);
+                        node.status({fill:"green", shape:"dot", text:"runtime.request-done"});
+                        msg.payload = res;
+                    } catch (error) {
+                        node.status({fill:"yellow", shape:"ring", text:error.message});
+                        msg.payload = error.message;
+                    } finally {
+                        node.send(msg);
+                    }
+                })();
+            }
+        });
+
+        this.on("close",function(done) {
+
+            // stop timers for the retry and the tapping
+            clearTimeout(cnctRtryId);
+            clearInterval(tappTimerId);
+
+            //非接続状態の時は、何もしない。
+            if (info.status === "Disconnected") return;
+            
+            (async () => {
+                // terminate request
+                try {
+                    let res = await iaC.terminate(auth);
+                    node.status({fill:"green", shape:"dot", text:"runtime.connected"});
+
+                } catch (error) {
+                    node.status({fill:"yellow", shape:"ring", text:error.message});
+
+                } finally {
+                    done();
+                }
+            })();
+        });
+    }
+    
+    RED.nodes.registerType("ia-cloud-cnct2",iaCloudCnct2,{
+        credentials: {
+            userID: {type:"text"},
+            password: {type: "password"}
+        }
+    });
+}
---
+++ ia-cloud-cnct/util/ia-cloud-connection.js
@@ -0,0 +1,342 @@
+/**
+ * Copyright JS Foundation and other contributors, http://js.foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+"use strict";
+const got = require('got');
+const moment = require("moment");
+
+class JsonParseError extends Error {
+    constructor(...args) {
+        super(...args);
+        this.name = this.constructor.name;
+        this.message = "Bad Response JSON";
+        this.code = "JSON_PARSE_ERR";
+        if (Error.captureStackTrace) Error.captureStackTrace(this, JsonParseError);
+    }
+}
+
+class IaCloudLowerError extends Error {
+    constructor(...args) {
+        super(...args);
+        this.name = this.constructor.name;
+        this.message = "ia-cloud Lower Protcol Error";
+        this.code = "IACLOUD_LOWER_ERR";
+        if (Error.captureStackTrace) Error.captureStackTrace(this, IaCloudLowerError);
+    }
+}
+
+class IaCloudAPIError extends Error {
+    constructor(...args) {
+        super(...args);
+        this.name = this.constructor.name;
+        this.message = "ia-cloud API Error";
+        this.code = "IACLOUD_API_ERR";
+        if (Error.captureStackTrace) Error.captureStackTrace(this, IaCloudAPIError);
+    }
+}
+
+class iaCloudConnection {
+
+    constructor( fContext, cnctInfoName,) {
+        this.fContext = fContext;
+        this.cnctInfoName = cnctInfoName;
+        this.cnctInfo = fContext.get(cnctInfoName);
+        let cnctInfo = this.cnctInfo;
+        // called with a connection info.
+        if (cnctInfo.hasOwnProperty("FDSKey")) {
+
+            // set http request options           
+            this.opts = {
+                url: cnctInfo.url,
+                method: "POST",
+                headers: {"Content-Type": "application/json"},
+                maxRedirects: 21,
+                proxy: cnctInfo.proxy,
+                reqTimeout: cnctInfo.reqTimeout
+            };   
+            
+        }
+        this.fContext.set(cnctInfoName, cnctInfo);
+    };
+
+    // a innternal method for http requests
+    async _iaCloudRequest(opts){
+        // make sharrow copy of opts
+        let options = {};
+        Object.assign(options, opts);
+        // delete url property from options
+        // The `url` option is mutually exclusive with the `input` argument
+        delete options["url"];
+        // other options for Got package
+        options.responseType = "text";
+
+        try {
+            const response = await got(opts.url, options);
+            if (response.statusCode === 200){
+                let resbody;
+                // Convert the JSON body to the object
+                try {resbody = JSON.parse(response.body); }
+                catch(e) { throw new JsonParseError(); }
+                return resbody;
+            } else {
+                // レスポンスコードが 200 OK ではない
+                throw new IaCloudLowerError();
+            }
+        } catch (err) {
+            if(err.code === 'ETIMEDOUT' || err.code === 'ESOCKETTIMEDOUT') {
+                throw new IaCloudLowerError();
+            }else{
+                throw err;
+            }
+        }
+    };
+
+    // a external method for a ia-cloud connect request
+    async connect(auth) {
+
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+        // connect リクエストのリクエストボディ
+        let reqbody = {
+            request: "connect",
+            userID: opts.username,
+            FDSKey: info.FDSKey,
+            FDSType: "iaCloudFDS",
+            timestamp: moment().format(),
+            comment: info.comment
+        };
+        opts.body = JSON.stringify(reqbody);
+
+        try {
+            let res = await this._iaCloudRequest(opts);
+
+            if (res.userID === reqbody.userID &&
+                res.FDSKey === reqbody.FDSKey && 
+                res.FDSType === reqbody.FDSType ) {
+
+                // ここで、serviceIDをconfiguration nodeである自身の接続情報にセットする
+                info.serviceID = res.serviceID;
+                info.status = "Connected";
+                info.cnctTs = moment().format();
+                return res;
+            } else {
+                throw new IaCloudAPIError();
+            }
+        } catch(error) {
+            info.serviceID = "";
+            info.status = "Disconnected";
+            throw error;
+        } finally {
+            this.fContext.set(this.cnctInfoName, info);
+        }
+    };
+
+    async getStatus(auth) {
+
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+        // getStatus リクエストのリクエストボディ
+
+        let reqbody = {
+            request: "getStatus",
+            serviceID: info.serviceID,
+            timestamp: moment().format(),
+            comment: info.comment
+        };
+        opts.body = JSON.stringify(reqbody);
+
+        try {
+            let res = await this._iaCloudRequest(opts);
+
+            if (res.serviceID === reqbody.serviceID 
+                    && res.FDSKey === info.FDSKey ) {
+
+                // ここで、serviceIDをconfiguration nodeである自身の接続情報にセットする
+                info.serviceID = res.newServiceID;
+                info.status = "Connected";
+                info.lastReqTs = moment().format();
+
+            } else {
+                throw new IaCloudAPIError();
+            }
+        } catch(error) {
+            info.serviceID = "";
+            info.status = "Disconnected";
+            throw error;
+        } finally {
+            this.fContext.set(this.cnctInfoName, info);
+        }
+
+    };
+
+    async store(auth, obj) {
+    
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+        // リクエストのリクエストボディ
+        let reqbody = {
+            request: "store",
+            serviceID: info.serviceID,
+            dataObject: obj
+        }
+        opts.body = JSON.stringify(reqbody);
+
+        try {
+            let res = await this._iaCloudRequest(opts);
+
+            if (res.serviceID === reqbody.serviceID && res.status.toLowerCase() === "ok" )  {
+
+                // ここで、serviceIDをconfiguration nodeである自身の接続情報にセットする
+                info.serviceID = res.newServiceID;
+                info.status = "Connected";
+                info.lastReqTs = moment().format();
+                return res;
+            } else {
+                throw new IaCloudAPIError();
+            }
+        } catch(error) {
+            info.serviceID = "";
+            info.status = "Disconnected";
+            throw error;
+        } finally {
+            this.fContext.set(this.cnctInfoName, info);
+        }
+    }
+
+    async retrieve(auth, obj) {
+
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+        // node status をReqesting に
+        info.status = "requesting";
+
+        // リクエストのリクエストボディ
+        let reqbody = {
+            request: "retrieve",
+            serviceID: info.serviceID,
+            retrieveObject: obj
+        }
+        opts.body = JSON.stringify(reqbody);
+
+        try {
+            let res = await this._iaCloudRequest(opts);
+
+            if (res.serviceID === reqbody.serviceID && res.status.toLowerCase() === "ok" )  {
+
+                // ここで、serviceIDをconfiguration nodeである自身の接続情報にセットする
+                info.serviceID = res.newServiceID;
+                info.status = "Connected";
+                info.lastReqTs = moment().format();
+                return res;
+            } else {
+                throw new IaCloudAPIError();
+            }
+        } catch(error) {
+            info.serviceID = "";
+            info.status = "Disconnected";
+            throw error;
+        } finally {
+            this.fContext.set(this.cnctInfoName, info);
+        }
+    };
+    
+    async convey(auth, obj) {
+
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+    };
+
+    async terminate(auth) {
+
+        let info = this.cnctInfo;
+
+        // id and pass must be a string 
+        if (typeof auth.user !== "string" || typeof auth.pass !== "string") {
+            throw new IaCloudAPIError();
+        };
+        let opts = this.opts;
+        opts.username = auth.user;
+        opts.password = auth.pass;
+
+        //terminateリクエストのリクエストボディ
+        let reqbody = {
+            request: "terminate",
+            serviceID: info.serviceID,
+        }
+        opts.body = JSON.stringify(reqbody);
+
+        try {
+            let res = await this._iaCloudRequest(opts);
+
+            if (res.userID === opts.username &&
+                res.FDSKey === info.FDSKey && 
+                res.serviceID === reqbody.serviceID ) {
+
+                // ここで、serviceIDをconfiguration nodeである自身の接続情報にセットする
+                info.serviceID = "";
+                info.status = "Disconnected";
+            } else {
+                throw new IaCloudAPIError();
+            }
+        } catch(error) {
+            throw error;
+        } finally {
+            this.fContext.set(this.cnctInfoName, info);
+        }
+    };
+
+}
+module.exports = iaCloudConnection;
