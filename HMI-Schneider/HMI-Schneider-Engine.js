696f6553a029df961f646fe5eaeb14a8d77d7400
a-shira <atsushi.shira.01.06@gmail.com>
2020-10-06 11:37:29 +0900

    Add engine classes which are common class for Schneider HMI nodes

---
+++ HMI-Schneider/HMI-Schneider-Engine.js
@@ -0,0 +1,304 @@
+
+var moment = require("moment");
+
+//  HMI-Schneider node implement class
+module.exports = HmiSchneiderEngine;
+
+function HmiSchneiderEngine(RED, owner, com, isvariable) {
+    this._RED = RED;
+    this._owner = owner;
+    this._com = com.getEngine();
+    this._isvariable = isvariable;
+
+    this._objects = [];
+
+    this.NodeStatus = {
+        Preparing: { fill: "blue", shape: "ring", text: "runtime.preparing" },
+        Disconnected: { fill: "red", shape: "dot", text: "runtime.disconnected" },
+        Connected: { fill: "green", shape: "dot", text: "runtime.connected" },
+        Sent: { fill: "green", shape: "dot", text: "runtime.sent" },
+    }
+
+    this._timerid = setInterval(intervalFunc.bind(this), 1000, this);
+}
+
+HmiSchneiderEngine.prototype.dispose = function () {
+    clearInterval(this._timerid);
+    this._com.delLinkData(this._owner.id);
+    this._com = null;
+    this._objects = [];
+}
+
+HmiSchneiderEngine.prototype.isconnected = function () {
+    if (this._com) {
+        return this._com.isconnected();
+    }
+    return false;
+}
+
+HmiSchneiderEngine.prototype.getNodeState = function () {
+    return this.isconnected() ? this.NodeStatus.Connected : this.NodeStatus.Disconnected;
+}
+
+HmiSchneiderEngine.prototype.addObjects = function (objects) {
+    let self = this;
+    objects.forEach(function (obj) { addObject_imp.call(self, obj); }, self);
+    addLink.call(self);
+}
+
+HmiSchneiderEngine.prototype.addObject = function (obj) {
+    let self = this;
+    addObject_imp.call(self, obj);
+    addLink.call(self);
+}
+
+HmiSchneiderEngine.prototype.replaceObjects = function (objects) {
+    this._objects = [];
+    this.addObjects(objects);
+}
+
+HmiSchneiderEngine.prototype.outputMsgs = function (msg) {
+    let self = this;
+
+    if (!this.isconnected()) {
+        this._owner.error("HMI is not connected.", msg);
+        return;
+    }
+
+    this._objects.forEach(function (obj) {
+        if (this._isvariable) {
+            sendVarMessages.call(self, obj, false);
+        } else {
+            sendAlarmMessages.call(self, obj, false);
+        }
+    }, this);
+}
+
+HmiSchneiderEngine.prototype.valueUpdated = function (variables) {
+    if (!this._isvariable) {
+        return;
+    }
+
+    this._objects.forEach(function (obj) {
+        obj.ObjectContent.contentData.forEach(function (dataItem) {
+            for (let i = 0; i < variables.length; i++) {
+                if (dataItem.varName == variables[i].name) {
+                    let value = (variables[i].quality != "good") ? null : variables[i].value;
+                    dataItem.value = value;
+                    break;
+                }
+            }
+        });
+    });
+}
+
+HmiSchneiderEngine.prototype.alarmUpdated = function (alarms) {
+    if (this._isvariable) {
+        return;
+    }
+
+    this._objects.forEach(function (obj) {
+        obj.ObjectContent.contentData.forEach(function (dataItem, index) {
+            for (let i = 0; i < alarms.length; i++) {
+                if (dataItem.varName == alarms[i].variable) {
+                    dataItem.status = (alarms[i].status == "Return") ? null : alarms[i].type;
+                    dataItem.message = alarms[i].message;
+                    break;
+                }
+            }
+        });
+    });
+}
+
+HmiSchneiderEngine.prototype.statusChanged = function () {
+    if (!this.isconnected()) {
+        //  HMIの接続が切れた場合はエラーとする
+        this._owner.error("HMI is not connected.");
+    }
+    this._RED.nodes.getNode(this._owner.id).emit("statusChanged");
+}
+
+function addObject_imp(_obj) {
+    let obj = Object.assign({}, _obj)
+    obj.ObjectContent = Object.assign({}, _obj.ObjectContent);
+    obj.ObjectContent.contentData = [];
+    _obj.ObjectContent.contentData.forEach(item => { obj.ObjectContent.contentData.push(Object.assign({}, item)); });
+
+    obj.lastIntervalCheck = null;
+    obj.lastValueChangedCheck = null;
+    obj.ObjectContent.contentData.forEach(item => {
+        if (this._isvariable) {
+            item.value = null;
+        } else {    //  alarm
+            item.status = null;
+            item.message = "";
+        }
+        item.prev = null;
+    });
+
+    this._objects.push(obj);
+}
+
+function addLink() {
+    let linkObj = { Items: [] };
+    linkObj.nodeId = this._owner.id;
+    linkObj.cb = this;
+    linkObj.kind = this._isvariable ? "variable" : "alarm";
+
+    if (this._isvariable) {
+        let items = [];
+        this._objects.forEach(function (obj) {
+            obj.ObjectContent.contentData.forEach(function (dataItem) {
+                items.push(dataItem.varName);
+            });
+        });
+        linkObj.Items = Array.from(new Set(items));
+    }
+
+    this._com.addLinkData(linkObj);
+}
+
+function intervalFunc() {
+    let self = this;
+    if (this._isvariable) {
+        intervalFuncVar.call(self);
+    }
+    else {
+        intervalFuncAlarm.call(self);
+    }
+}
+
+function intervalFuncVar() {
+    let self = this;
+    let current = Date.now();
+
+    self._objects.forEach(function (obj) {  //  check interval
+        if (obj.storeInterval > 0) {
+            if ((obj.lastIntervalCheck == null) || (current - (obj.lastIntervalCheck) >= (obj.storeInterval * 1000))) {
+                sendVarMessages.call(self, obj, (obj.lastIntervalCheck == null)); //  初回だけ変化通知のフラグをONする
+                obj.lastIntervalCheck = current;
+            }
+        }
+    }, self);
+
+    self._objects.forEach(function (obj) {  //  check async
+        if (obj.asyncInterval > 0) {
+            if ((obj.lastValueChangedCheck == null) || (current - (obj.lastValueChangedCheck) >= (obj.asyncInterval * 1000))) {
+                obj.lastValueChangedCheck = current;
+                if (!haveVarsUpdated.call(self, obj.ObjectContent.contentData)) {
+                    return;
+                }
+                sendVarMessages.call(self, obj, true);
+            }
+        }
+    }, self);
+}
+
+function haveVarsUpdated(items) {
+    let self = this;
+    if (!self.isconnected()) {
+        return false;
+    }
+    return (items.find(item => item.value != item.prev) != undefined) ? true : false;
+}
+
+function sendVarMessages(obj, valuechanged) {
+    if (!this.isconnected()) {
+        //  HMIが接続されていない場合は何もしない
+        return false;
+    }
+
+    let msg = createMsg(obj, true);
+    if (valuechanged) { //  update previous value when value changed trigger
+        obj.ObjectContent.contentData.forEach(item => { item.prev = item.value; });
+    }
+
+    this._RED.nodes.getNode(this._owner.id).emit("outputMsg", msg);
+
+    return true;
+}
+
+function intervalFuncAlarm() {
+    let self = this;
+    let current = Date.now();
+
+    self._objects.forEach(function (obj) { //  check interval
+        if (obj.storeInterval > 0) {
+            if ((obj.lastIntervalCheck == null) || (current - (obj.lastIntervalCheck) >= (obj.storeInterval * 1000))) {
+                sendAlarmMessages.call(self, obj, (obj.lastIntervalCheck == null)); //  初回だけ変化通知のフラグをONする
+                obj.lastIntervalCheck = current;
+            }
+        }
+    }, self);
+
+    self._objects.forEach(function (obj) { //  check async
+        if (obj.asyncInterval > 0) {
+            if ((obj.lastValueChangedCheck == null) || (current - (obj.lastValueChangedCheck) >= (obj.asyncInterval * 1000))) {
+                obj.lastValueChangedCheck = current;
+                if (!haveAlarmsUpdated.call(self, obj.ObjectContent.contentData)) {
+                    return;
+                }
+                sendAlarmMessages.call(self, obj, true);
+            }
+        }
+    }, self);
+}
+
+function haveAlarmsUpdated(items) {
+    return (items.find(item => item.status != item.prev) != undefined) ? true : false;
+};
+
+function sendAlarmMessages(obj, valuechanged) {
+    let msg = createMsg(obj, false);
+    if (valuechanged) { //  update previous value when value changed trigger
+        obj.ObjectContent.contentData.forEach(item => { item.prev = item.status; });
+    }
+
+    this._RED.nodes.getNode(this._owner.id).emit("outputMsg", msg);
+
+    return true;
+}
+
+function createMsg(obj, isvariable) {
+    let msg = { request: "store", dataObject: { ObjectContent: {} } };
+
+    msg.dataObject.objectKey = obj.objectKey;
+    msg.dataObject.timeStamp = moment().format();
+    msg.dataObject.objectType = "iaCloudObject";
+    msg.dataObject.objectDescription = obj.objectDescription;
+    msg.dataObject.ObjectContent.contentType = obj.ObjectContent.contentType;
+
+    let contentData = createContendData(obj, isvariable);
+    msg.dataObject.ObjectContent.contentData = contentData;
+    msg.payload = contentData;
+
+    return msg;
+}
+
+function createContendData(obj, isvariable) {
+    let contentData = [];
+    obj.ObjectContent.contentData.forEach(function (item) {
+        let dItem = {};
+        if (isvariable) {
+            dItem.dataName = item.dataName;
+            dItem.dataValue = item.value;
+            if (item.unit && (item.unit != "")) {
+                dItem.unit = item.unit;
+            }
+        }
+        else {
+            if (item.status == null) {
+                dItem.AnEStatus = (item.status == item.prev) ? "off" : "reset";
+            } else {
+                dItem.AnEStatus = (item.status == item.prev) ? "on" : "set";
+            }
+            dItem.AnECode = item.code;
+            if (!dItem.AnECode) { dItem.AnECode = item.varName; }
+            dItem.AnEDescription = item.description;
+            if (!dItem.AnEDescription) { dItem.AnEDescription = item.message; }
+        }
+        contentData.push(dItem);
+    });
+
+    return contentData;
+}
--- HMI-Schneider/HMI-Schneider-com.js
+++ HMI-Schneider/HMI-Schneider-com.js
@@ -1,144 +1,25 @@
 
+var ComEngine = require('./HMI-Schneider-ComEngine');
+
 module.exports = function (RED) {
     "use strict";
 
     function hmiSchneiderCom(config) {
         RED.nodes.createNode(this, config);
 
-        const HmiSchneiderWebSocket = require('./HMI-Schneider-Websocket.js');
+        this._engine = new ComEngine("ws://" + config.IPAdd + ":" + config.TCPPort + "/api/v1/ws");
 
-        this.host = config.IPAdd;
-        this.port = config.TCPPort;
-
-        this.subscription = false;
-        this.ws = null;
-
-        this.linkObjs = [];
-        this.flagRecon = false;
-        this.flagOpened = false;
-
-        this.startmonitor = function () {
-            if ((this.ws != null) && (this.flagOpened == true)) {
-                this.ws.send_clear_monitor();
-
-                let addList = [];
-                this.linkObjs.forEach(function (linkObj, idx) {
-                    if ((linkObj != null) && (linkObj.kind == "variable")) {
-                        linkObj.Items.forEach(function (variable, idx) {
-                            addList.push(variable);
-                        });
-                    }
-                });
-                this.ws.send_add_monitor(addList);
-
-                if (this.flagRecon) {
-                    // 通信フレーム情報の再構成フラグをoff
-                    this.flagRecon = false;
-                }
-            }
-        };
-
-        this.ws = new HmiSchneiderWebSocket();
-
-        this.ws.onopen = function () {
-            this.flagOpened = true;
-            this.subscription = false;
-            this.startmonitor();
-            this.statusUpdate(true);
-        }.bind(this);
-
-        this.ws.onclose = function () {
-            if (this.flagOpened == true) {
-                this.flagOpened = false;
-                this.statusUpdate(false);
-            }
-        }.bind(this);
-
-        this.ws.onmessage = function (jsonObj) {
-            if (this.subscription == false) {
-                this.subscription = true;
-            }
-
-            let updated = jsonObj.updated;
-            if (updated == "variable") {
-                this.variableUpdate(jsonObj.data);
-            }
-            else if (updated == "alarm") {
-                this.alarmUpdate(jsonObj.data);
-            }
-            else if (updated == "error") {
-            }
-        }.bind(this);
-
-        this.ws.open("ws://" + this.host + ":" + this.port + "/api/v1/ws", undefined);
-
-        this.variableUpdate = function (variables) {
-            this.linkObjs.forEach(function (linkObj) {
-                if ((linkObj != null) && (linkObj.kind == "variable")) {
-                    let hmiNode = RED.nodes.getNode(linkObj.nodeId);
-                    if (hmiNode)
-                        hmiNode.emit("valueUpdated", variables);
-                }
-            });
-        };
-
-        this.alarmUpdate = function (alarms) {
-            this.linkObjs.forEach(function (linkObj) {
-                if ((linkObj != null) && (linkObj.kind == "alarm")) {
-                    let hmiNode = RED.nodes.getNode(linkObj.nodeId);
-                    if (hmiNode)
-                        hmiNode.emit("alarmUpdated", alarms);
-                }
-            });
-        };
-
-        this.statusUpdate = function (connected) {
-            this.linkObjs.forEach(function (linkObj) {
-                if (linkObj != null) {
-                    let hmiNode = RED.nodes.getNode(linkObj.nodeId);
-                    if (hmiNode)
-                        hmiNode.emit("statusChanged", connected);
-                }
-            });
-        };
+        this.getEngine = function () {
+            return this._engine;
+        }
 
         this.on("input", function (msg) {
-
         });
 
         this.on("close", function () {
-            if (this.ws != null) {
-                this.flagOpened = false;
-                this.ws.dispose();
-            }
-        });
-
-        this.on("addLinkData", function (lObj) {
-            let found = false;
-            for (let i = 0; i < this.linkObjs.length; i++) {
-                if ((this.linkObjs[i] != null) && (this.linkObjs[i].nodeId == lObj.nodeId)) {
-                    this.linkObjs[i] = lObj;
-                    found = true;
-                    break;
-                }
-            }
-
-            if (found == false) {
-                this.linkObjs.push(lObj);
-            }
-            // linkObjが変更されたので、通信フレーム情報の再構築フラグをon
-            this.flagRecon = true;
-
-            this.startmonitor();
-        });
-
-        this.on("delLinkData", function (nodeId) {
-            let len = this.linkObjs.length;
-            this.linkObjs = this.linkObjs.filter(obj => obj.nodeId != nodeId);
-            if (len != this.linkObjs.length) {
-                // linkObjが変更されたので、通信フレーム情報の再構築フラグをon
-                this.flagRecon = true;
-                this.startmonitor();
+            if (this._engine) {
+                this._engine.dispose();
+                this._engine = null;
             }
         });
     }
